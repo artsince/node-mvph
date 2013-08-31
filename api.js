596b43ece948d3f116659cc53377bc01d46abbaa
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2013-08-31 19:54:22 +0300

    added random and get method for pictures

--- api.js
+++ api.js
@@ -1,3 +1,57 @@
+var http = require('http');
+
+var downloadImage = function (id, res) {
+    var httpGetOptions = {
+            host: 'tbmm.gov.tr',
+            port: 80,
+            path: '/mv_resim/' + id + '.jpg',
+            method: 'GET'
+        };
+
+        var req = http.request(httpGetOptions, function (tbmmRes) {
+            if(tbmmRes.statusCode !== 200) {
+                res.status(404).send();
+                return;
+            }
+
+            var offset = 0;
+            var tbmmResLen = parseInt(tbmmRes.headers['content-length']);
+            var tbmmResContentType = tbmmRes.headers['content-type'];
+            var imgBuffer = new Buffer(tbmmResLen);
+
+            tbmmRes.setEncoding('binary');
+
+            tbmmRes.on('data', function (chunk) {
+                imgBuffer.write(chunk, offset, 'binary');
+                offset += chunk.length;
+            });
+
+            tbmmRes.on('end', function () {
+                res.contentType = tbmmResContentType;
+                res.send(imgBuffer);
+            });
+        });
+
+        req.on('error', function (err) {
+            console.log(err);
+            res.status(500).send();
+            return;
+        });
+
+    req.end();
+};
+
 exports.random = function (req, res) {
-    res.status(200).send({msg: 'it works!'});
+   var id = 1 + Math.floor(Math.random() * 6000);
+   downloadImage(id, res);
+};
+
+exports.getMP = function (req, res) {
+    var id = req.params.id || undefined;
+    if(id === undefined) {
+        res.status(400).send();
+        return;
+    }
+
+    downloadImage(id, res);
 };
--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var api = require('./api');
-var app = express();
+var application_root = __dirname;
+var path = require('path');
+var app = express(); 
 
 app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
+    app.use(express.static(application_root + '/public'));
 });
 
 app.get('/random', api.random);
+app.get('/p/:id', api.getMP);
 
 app.listen(process.env.PORT || 4242);
 
