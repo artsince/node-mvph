e695531c6e4f117dcc7fbef65d6dfb5b7d4a231b
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2013-09-01 17:05:08 +0300

    restructured main routines

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var api = require('./api');
-var application_root = __dirname;
-var path = require('path');
-var app = express(); 
+(function () {
+    var express = require('express');
+    var api = require('./api');
+    var application_root = __dirname;
+    var path = require('path');
+    var app = express(); 
 
-app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(application_root + '/public'));
-});
+    app.configure(function () {
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(application_root + '/public'));
+    });
 
-app.get('/random', api.random);
-app.get('/p/:id', api.getMP);
+    app.get('/random', api.random);
+    app.get('/p/:id', api.getMP);
 
-app.listen(process.env.PORT || 4242);
+    app.listen(process.env.PORT || 4242);
 
-module.exports = app;
+}).call(this);
+
